b1370da3d85104f13c9a4df11be8f64984672061
Lew Savage <LS@savagesoundsinc.com>
2023-12-01 21:58:09 -0500

    adding basic html structure and definitions

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>Footer
+    {/* Todos
+
+        Contact Info
+        SOcial Links
+        Dormant Nav links
+        Logo
+
+    
+     */}
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <header>
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+      <p>Lorem ipsum</p>
+      <button>Reserve A Table</button>
+      <img alt='table' />
+    </header>
+  )
+}
+
+export default Hero
